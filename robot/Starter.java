a05dffd153e93f0e97937b72a18f897f6296d9e4
Sahil Deshmukh <113948758+UndercoverAtSchool@users.noreply.github.com>
2025-08-15 06:29:39 +0000

    Add P2 PBM and imageio P4 PBM support
    NOTE: imageio jars are not included in commit
    See settings.json for updated classpath

--- robot/Starter.java
+++ robot/Starter.java
@@ -1,13 +1,106 @@
 package robot;
 
 import kareltherobot.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+// https://repo1.maven.org/maven2/com/twelvemonkeys/imageio/imageio-pnm/3.12.0/imageio-pnm-3.12.0.jar
 
 public class Starter implements Directions {
 
-  public static boolean containsArray(int[][] multiArray, int[] targetArray) { // util because I ain't writing allat
-                                                                               // later
+  private int[][] data;
+
+  public int dimX;
+  public int dimY;
+
+  public void readPbm(String filename) throws IOException {
+    Scanner sc = new Scanner(new File(filename));
+    String format = sc.nextLine();
+    if (!format.equals("P2")) {
+      sc.close();
+      throw new IOException("Format not supported");
+    }
+
+    // comments mess with me
+    // String line = sc.nextLine();
+    // while (line.startsWith("#")) {
+    // line = sc.nextLine();
+    // }
+    String line = sc.nextLine();
+    String[] dims = line.trim().split("\\s+"); // trim space
+    int width, height;
+    if (dims.length == 2) {
+      width = Integer.parseInt(dims[0]);
+      height = Integer.parseInt(dims[1]);
+    } else {
+      width = Integer.parseInt(dims[0]);
+      height = Integer.parseInt(sc.nextLine().trim());
+    }
+    dimX = width;
+    dimY = height;
+
+    int maxval = Integer.parseInt(sc.nextLine().trim());
+
+    data = new int[height][width];
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        data[y][x] = sc.nextInt();
+      }
+    }
+    sc.close();
+
+    // pad to square
+    int dimSquare = Math.max(dimX, dimY);
+    if (dimX != dimY) {
+      int[][] padded = new int[dimSquare][dimSquare];
+      for (int y = 0; y < dimY; y++) {
+        for (int x = 0; x < dimX; x++) {
+          padded[y][x] = data[y][x];
+        }
+      }
+      data = padded;
+      dimX = dimSquare;
+      dimY = dimSquare;
+    }
+  }
+
+  // uses all 12 monkeys
+  public void readPbmP4(String filename) throws IOException {
+    BufferedImage img = ImageIO.read(new File(filename));
+    if (img == null) {
+      throw new IOException("Could not read file");
+    }
+    dimX = img.getWidth();
+    dimY = img.getHeight();
+    data = new int[dimY][dimX];
+    for (int y = 0; y < dimY; y++) {
+      for (int x = 0; x < dimX; x++) {
+        int rgb = img.getRGB(x, y) & 0xFFFFFF;
+        data[y][x] = (rgb == 0x000000) ? 1 : 0;
+      }
+    }
+    // pad to square
+    int dimSquare = Math.max(dimX, dimY);
+    if (dimX != dimY) {
+      int[][] padded = new int[dimSquare][dimSquare];
+      for (int y = 0; y < dimY; y++) {
+        for (int x = 0; x < dimX; x++) {
+          padded[y][x] = data[y][x];
+        }
+      }
+      data = padded;
+      dimX = dimSquare;
+      dimY = dimSquare;
+    }
+  }
+
+  public static boolean containsArray(int[][] multiArray, int[] targetArray) {
     if (multiArray == null || targetArray == null) {
       return false;
     }
@@ -20,72 +113,52 @@ public class Starter implements Directions {
     return false;
   }
 
-  public static void main(String[] args) {
-
-    World.setVisible(true);// allows us to see the run output
-    // the bigger the street, the farther north
-    World.setSize(20, 20);
-    World.setDelay(1);
-
-    // The line below creates a Robot that we will refer to as rob
-    // Find out what the numbers and direction do!
-    // Put rob in a better location for your initials.
-    // Robot rob = new Robot(15,2,South,9); // why the hell is it inverted??
-
-    int[][] posconstruct = new int[20][];
-    for (int i = 0; i < posconstruct.length; i++) {
-      posconstruct[i] = new int[0];
-    }
-    posconstruct[0] = new int[] { 1, 2, 3, 4 };
-    posconstruct[5] = new int[] { 7, 8 };
-
-    int[][] pos = { { 0, 0 }, { 1, 2 } };
-    Robot[] bots = new Robot[20];
-
-    for (int i = 0; i < 20; i++) {
-      bots[i] = new Robot(1, 1 + i, North, 20);
-      // for (int j = 0; j < 20; j++) {
-      // bots[i].putBeeper();
-      // bots[i].move();
-      // }
-    }
-    for (int i = 0; i < 20; i++) {
-      for (int j = 0; j < 20; j++) {
-        final int index = i;
-        // j approx x, i approx y
-        // if (containsArray(pos, new int[] { j, i })) {
-        // bots[j].putBeeper();
-        // }
-        if (IntStream.of(posconstruct[j]).anyMatch(x -> x == index)) {
-          bots[j].putBeeper();
-        }
-
-        bots[j].move();
+  public static boolean containsInt(int[] miniArray, int target) {
+    for (int item : miniArray) {
+      if (item == target) {
+        return true;
       }
     }
+    return false;
+  }
 
-    // Want a second robot? No prob. They are cheap :)
-    // Robot dude = new Robot(7,5,West,9);
-    // examples of commands you can invoke on a Robot
-    // rob.move();// move one step in the direction it is facing
+  public static void main(String[] args) {
 
-    // // starting the letter R
-    // rob.putBeeper();
-    // rob.putBeeper();
-    // rob.putBeeper();
-    // rob.putBeeper();
-    // rob.move();
-    // rob.putBeeper();
-    // rob.move();
-    // rob.move();
-    // rob.move();
-    // rob.putBeeper();
-    // rob.move();
-    // rob.move();
-    // rob.putBeeper();
+    Starter starter = new Starter();
+    try {
+      starter.readPbmP4("robot/data/1f928.pbm");
+    } catch (IOException e) {
+      System.err.println("reading error (probably file): " + e.getMessage());
+      e.printStackTrace();
+      return;
+    } catch (Exception e) {
+      System.err.println("unexpected error: " + e.getMessage());
+      e.printStackTrace();
+      return;
+    }
 
-    // done with the line, now on the curve
-    // rob.turnLeft();
+    int dimSquare = Math.max(starter.dimX, starter.dimY);
+    System.out.println(dimSquare);
 
+    World.setVisible(true);
+    World.setSize(dimSquare, dimSquare);
+    World.setDelay(0);
+
+    int[][] posconstruct = starter.data;
+
+    Robot[] bots = new Robot[dimSquare];
+
+    for (int i = 0; i < dimSquare; i++) {
+      bots[i] = new Robot(1, 1 + i, North, dimSquare);
+    }
+
+    for (int j = 0; j < dimSquare; j++) {
+      for (int i = 0; i < dimSquare; i++) {
+        if (posconstruct[dimSquare - 1 - j][i] == 1) {
+          bots[i].putBeeper();
+        }
+        bots[i].move();
+      }
+    }
   }
 }
