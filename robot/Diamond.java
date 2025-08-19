5c0b055792854106d517a5a2e18550e5ae7a2362
Sahil Deshmukh <113948758+UndercoverAtSchool@users.noreply.github.com>
2025-08-19 21:10:53 +0000

    WIP - diamond

--- robot/Diamond.java
+++ robot/Diamond.java
@@ -1,34 +1,96 @@
 package robot;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import kareltherobot.*;
 
+class Point {
+    public int x;
+    public int y;
 
-public class Diamond implements Directions{
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Diamond implements Directions {
+
+    private List<Point> getLinePoints(Point p1, Point p2) {
+        // construct range of points to search
+        // for each point - if dist is <0.5 treat point as part of line
+        int xMin = Math.min(p1.x, p2.x);
+        int xMax = Math.max(p1.x, p2.x);
+        int yMin = Math.min(p1.y, p2.y);
+        int yMax = Math.max(p1.y, p2.y);
+
+        int a = p1.y - p2.y;
+        int b = p2.x - p1.x;
+        int c = p1.x * p2.y - p2.x * p1.y;
+        double denom = Math.sqrt(a * a + b * b);
+        // d = |ax₁ + by₁ + c| / √(a² + b²)
+        // ^^ formula for distance to line of form ax + by + c = 0
+
+        List<Point> points = new ArrayList<>();
+        for (int i = xMin; i <= xMax; i++) {
+            for (int j = yMin; j <= yMax; j++) {
+                double dist = (denom == 0) ? 0 : (Math.abs(a * i + b * j + c) / denom);
+                if (dist < 0.5) {
+                    points.add(new Point(i, j));
+                }
+            }
+        }
+        // pointmap
+        return points;
+    }
+
+    private void drawLineFromPoints(List<Point> pointList) {
+        Robot linerob = new Robot(pointList.get(0).y, pointList.get(0).x, South, 100);
+        Direction[] dirs = new Direction[2];
+
+        if (pointList.getLast().x - pointList.getFirst().x > 0) {
+            dirs[0] = East;
+        } else {
+            dirs[0] = West;
+        }
+
+        if (pointList.getLast().y - pointList.getFirst().y > 0) {
+            dirs[0] = North;
+        } else {
+            dirs[0] = South;
+        }
+        // linerob - spawn at first point, go through points, move vertical diff num
+        // times, move horizontal diff num times
+
+        Direction currentDir = South;
+        Point currentPos = new Point(pointList.get(0).y, pointList.get(0).x);
+
+        int currentIndex = 1;
+        for (Point point : pointList) {
+            while (!(dirs[0] == currentDir)) {
+                linerob.turnLeft();
+            }
+            // horizontally aligned
+            // while (!(currentPos.x) ==
+
+        }
+
+    }
 
     public static void main(String[] args) {
 
-        World.setVisible(true);// allows us to see the run output
-        // the bigger the street, the farther north
-        World.setSize(20,20);
+        Diamond diamond = new Diamond();
 
+        World.setVisible(true);
+        World.setSize(20, 20);
 
-        // The line below creates a Robot that we will refer to as rob
-        // Find out what the numbers and direction do!
-        // Put rob in a better location for your initials.
-        Robot rob = new Robot(15,2,South,9);
+        Robot rob = new Robot(15, 2, South, 9);
 
-        // Want a second robot?  No prob.  They are cheap :)
-        //Robot dude = new Robot(7,5,West,9);
-        // examples of commands you can invoke on a Robot
-        rob.move();// move one step in the direction it is facing
-
-
-        // starting the letter R
-        rob.putBeeper();
-
-
-
-        // done with the line, now on the curve
-        // rob.turnLeft();
+        // draw from (2, 3) to (5, 8) to test
+        List<Point> points = diamond.getLinePoints(new Point(2, 3), new Point(4, 6));
+        System.out.print(points.get(0).x);
 
     }
 }
