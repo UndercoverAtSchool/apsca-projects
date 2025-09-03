561ed92838d66193a65c3dfcbba4dc00c3cc2663
Sahil Deshmukh <113948758+UndercoverAtSchool@users.noreply.github.com>
2025-09-03 18:20:36 +0000

    Simplified + removed debug logging

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -24,38 +24,21 @@ public class Roomba implements Directions {
 	// declared here so it is visible in all the methods!
 	private Robot roomba;
 
-	// You will need to add many variables!!
-
 	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance)
-		// variable named roomba that is declared above.
-		// Make sure it starts at startX and startY location.
 
 		World.readWorld(worldName);
 		World.setVisible(true);
 		World.setDelay(2);
 
-		/**
-		 * This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
 		roomba = new Robot(startX, startY, North, 0);
 		roomba.move();
 
 		// first move to top left of box to normalize position
 		for (int i = 0; i < 2; i++) { // up, then left
-			while (true) {
-				if (!roomba.frontIsClear()) {
-					roomba.turnLeft();
-					break;
-				}
+			while (roomba.frontIsClear()) {
 				roomba.move();
 			}
+			roomba.turnLeft();
 
 		}
 
@@ -66,34 +49,26 @@ public class Roomba implements Directions {
 		// facing East in top left corner
 
 		while (true) {
-			while (true) {
-				if (!roomba.nextToABeeper()) {
-					break;
-				}
+			while (roomba.nextToABeeper()) {
 				roomba.pickBeeper();
 				totalBeepers++;
 			}
 
 			if (!roomba.frontIsClear()) {
-				System.out.println("Detected wall collision");
 				if (roomba.facingEast()) {
-					System.out.println("ran into a wall facing EAST");
 					turnRight(roomba);
 					if (!roomba.frontIsClear()) {
 						break;
 					}
 					roomba.move();
 					turnRight(roomba);
-					System.out.println(roomba.direction());
 				} else if (roomba.facingWest()) {
-					System.out.println("ran into a wall facing WEST");
 					roomba.turnLeft();
 					if (!roomba.frontIsClear()) {
 						break;
 					}
 					roomba.move();
 					roomba.turnLeft();
-					System.out.println(roomba.direction());
 				}
 			}
 			roomba.move();
