b5d92d22654f137212963bbff0f11783d94be9ff
Sahil Deshmukh <113948758+UndercoverAtSchool@users.noreply.github.com>
2025-10-24 21:11:33 +0000

    Update App, final commit

--- piglatin/App.java
+++ piglatin/App.java
@@ -4,19 +4,23 @@ public class App {
     public static void main(String[] args) {
 
         // int score = TestSuite.run();
-        System.out.println("hi");
 
         if (true) {
             Book input = new Book();
 
-            input.readFromUrl("Romeo and Juliette",
-                    "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
+            final String bookTitle = "Romeo and Juliette"; // La figlia di Jefte
+            final String fileName = "omeoray"; // igliafay
+            final int bookId = 1513; // 1513, 77118
+            // https://www.gutenberg.org/cache/epub/77118/pg77118.txt
 
-            input.printlines(0, 2);
-            Book output = PigLatinTranslator.translate(input);
-            output.printlines(0, 2);
-            output.writeToFile("output/omeoray.txt");
+            input.readFromUrl(bookTitle,
+                    String.format("https://gutenberg.pglaf.org/cache/epub/%1$d/pg%1$d.txt", bookId));
+
             input.writeToFile("output/original.txt");
+
+            Book output = PigLatinTranslator.translate(input);
+
+            output.writeToFile(String.format("output/%s.txt", fileName));
         }
     }
 }
