71d2c1db17c2e3f0c1857172aa74247aaeb0ab31
Sahil Deshmukh <hello@sahil.ink>
2026-01-26 13:59:19 -0800

    Actually implement insertion sort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,6 +3,18 @@ package sorting;
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        for (int i = 0; i < input.length; i++) {
+            int val = input[i];
+            int insertAt = 0;
+            while (val >= input[insertAt] && insertAt < i) {
+                insertAt++;
+            }
+            int temp = input[i];
+            // rotate from indices insertAt -> i
+            for (int j = insertAt; j < i; j++) {
+                input[j + 1] = input[j];
+            }
+            input[insertAt] = temp;
+        }
     }
 }
