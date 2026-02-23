7112f92022283b850dde43b96b7f67bfd5488b01
Sahil Deshmukh <hello@sahil.ink>
2026-02-23 14:10:40 -0800

    Working irregular polygon

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -8,33 +8,48 @@ public class IrregularPolygon {
     private ArrayList<Point2D.Double> myPolygon = new ArrayList<Point2D.Double>();
 
     // constructor
-    public IrregularPolygon() {}
+    public IrregularPolygon() {
+    }
 
     // public methods
-    public void add(Point2D.Double aPoint)
-    {
-        // TODO: Add a point to the IrregularPolygon.
+    public void add(Point2D.Double aPoint) {
+        myPolygon.add(aPoint);
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+        double p = 0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            p += Math.sqrt(
+                    Math.pow((myPolygon.get(i).x - myPolygon.get((i + 1) % myPolygon.size()).x), 2) +
+                            Math.pow((myPolygon.get(i).y - myPolygon.get((i + 1) % myPolygon.size()).y), 2));
+        }
+        return p;
     }
 
     public double area() {
-        // TODO: Calculate the area.
-        Double area = 0.0;
-        return area;
+        Double intermediate = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            intermediate += (myPolygon.get(i).x * myPolygon.get((i + 1) % myPolygon.size()).y);
+            intermediate -= (myPolygon.get(i).y * myPolygon.get((i + 1) % myPolygon.size()).x);
+        }
+        return Math.abs(intermediate / 2);
     }
 
-    public void draw()
-    {
-        // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
+    public void draw() {
+        // Wrap the DrawingTool in a try/catch to allow development without need for
+        // graphics.
         try {
-            // TODO: Draw the polygon.
             // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
-            //DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
-            //myDrawingTool.move(50, 50);
+            DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
+            if (myPolygon.isEmpty())
+                return;
+            myDrawingTool.up();
+            myDrawingTool.move(myPolygon.get(0).x, myPolygon.get(0).y);
+            myDrawingTool.down();
+            for (int i = 1; i < myPolygon.size(); i++) {
+                myDrawingTool.move(myPolygon.get(i).x, myPolygon.get(i).y);
+            }
+            myDrawingTool.move(myPolygon.get(0).x, myPolygon.get(0).y);
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
--- polygon/Main.java
+++ polygon/Main.java
@@ -1,12 +1,18 @@
 package polygon;
 
+import java.awt.geom.Point2D;
+
 public class Main {
-    public static void main(String [] args)
-    {
-        IrregularPolygon myPolygon = new IrregularPolygon();
-        myPolygon.draw();
+    public static void main(String[] args) {
+        // IrregularPolygon myPolygon = new IrregularPolygon();
+        // myPolygon.add(new Point2D.Double(0, 0));
+        // myPolygon.add(new Point2D.Double(50, 50));
+        // myPolygon.add(new Point2D.Double(25, 60));
+        // myPolygon.add(new Point2D.Double(15, 70));
+        // myPolygon.add(new Point2D.Double(0, 80));
+        // myPolygon.draw();
 
         TestSuite.run();
     }
-    
+
 }
